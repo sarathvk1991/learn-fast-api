7d6104a7a6edb23efa0bf40aa4d3a5c5ab6c6137
Sarath.V <sarathvk619@gmail.com>
2026-03-17 13:32:54 +0530

    using custom filter to hide sensitive data

--- socialmediaapi/logging_conf.py
+++ socialmediaapi/logging_conf.py
@@ -1,6 +1,24 @@
+import logging
 from logging.config import dictConfig
 
 from socialmediaapi.config import DevConfig, config
+
+
+def obfuscate_email(email: str, obfuscated_length: int = 2) -> str:
+    characters = email[:obfuscated_length]
+    first, last = email.split("@")
+    return characters + ("*" * (len(first) - obfuscated_length)) + "@" + last
+
+
+class EmailObfuscationFilter(logging.Filter):
+    def __init__(self, name: str = "", obfuscated_length: int = 2) -> None:
+        super().__init__(name)
+        self.obfuscated_length = obfuscated_length
+
+    def filter(self, record: logging.LogRecord) -> bool:
+        if "email" in record.__dict__:
+            record.email = obfuscate_email(record.email)
+        return True
 
 
 def configure_logging():
@@ -13,7 +31,11 @@ def configure_logging():
                     "()": "asgi_correlation_id.CorrelationIdFilter",
                     "uuid_length": 8 if isinstance(config, DevConfig) else 32,
                     "default_value": "-",
-                }
+                },
+                "email_obfuscation": {
+                    "()": EmailObfuscationFilter,
+                    "obfuscated_length": 2 if isinstance(config, DevConfig) else 0,
+                },
             },
             "formatters": {
                 "console": {
@@ -32,7 +54,7 @@ def configure_logging():
                     "class": "rich.logging.RichHandler",
                     "level": "DEBUG",
                     "formatter": "console",
-                    "filters": ["correlation_id"],
+                    "filters": ["correlation_id", "email_obfuscation"],
                 },
                 "rotating_file": {
                     "class": "logging.handlers.RotatingFileHandler",
@@ -42,7 +64,7 @@ def configure_logging():
                     "maxBytes": 1024 * 1024,  # 1 MB
                     "backupCount": 5,
                     "encoding": "utf-8",
-                    "filters": ["correlation_id"],
+                    "filters": ["correlation_id", "email_obfuscation"],
                 },
             },
             "loggers": {
