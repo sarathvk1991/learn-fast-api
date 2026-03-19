f09c863c1cea119b99c6953c9977c7ebf70be22d
Sarath.V <sarathvk619@gmail.com>
2026-03-19 15:52:34 +0530

    API Key for image generation using deep ai. Key to be generated after registering with deepai.com

--- socialmediaapi/config.py
+++ socialmediaapi/config.py
@@ -18,6 +18,7 @@ class GlobalConfig(BaseConfig):
     B2_KEY_ID: Optional[str] = None
     B2_APPLICATION_KEY: Optional[str] = None
     B2_BUCKET_NAME: Optional[str] = None
+    DEEP_AI_API_KEY: Optional[str] = None
 
     # config: ClassVar[BaseConfig] = BaseConfig()
 
